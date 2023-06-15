a2c877c1b75927552184cddaa1c93635c11202a3
Gabriel Henrique <gabrielhenrique16041999@gmail.com>
2023-06-15 19:27:08 -0300

    commit

---
+++ src/main/java/com/io/blog/controller/AutorController.java
@@ -0,0 +1,48 @@
+package com.io.blog.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.io.blog.model.Autor;
+import com.io.blog.service.AutorService;
+
+@CrossOrigin(origins = "http://localhost:5173")
+@RestController
+@RequestMapping("/autor")
+public class AutorController {
+	
+	@Autowired
+	private AutorService serviceAutor;
+	
+	@PostMapping("/salvar")
+	public String salvar(@RequestBody Autor autor) {
+		try {
+			System.out.println(autor);
+			serviceAutor.cadastrarAutor(autor);
+			return "{\"status\":\"Usuário cadastrado com sucesso\"}";
+		} catch (Exception e) {
+			return "{\"status\":\"Erro no cadastro\"}";
+		}
+
+	}
+	
+	@PostMapping("/login")
+	public String login(@RequestBody Autor autor) {
+	    Autor autorEncontrado = serviceAutor.buscarAutorPorMatricula(autor.getMatricula());
+
+	    if (autorEncontrado != null && autorEncontrado.getSenha().equals(autor.getSenha())) {
+	        return "{\"status\":\"Login bem-sucedido\"}";
+	    } else {
+	        return "{\"status\":\"Credenciais inválidas\"}";
+	    }
+	}
+	
+}
+
---
+++ src/main/java/com/io/blog/repository/AutorRepository.java
@@ -0,0 +1,12 @@
+package com.io.blog.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.io.blog.model.Autor;
+
+@Repository
+public interface AutorRepository extends JpaRepository<Autor, Long> {
+	Autor findByMatricula(String matricula);
+
+}
